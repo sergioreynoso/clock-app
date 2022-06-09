41a228354abdb75b61b1c23ab8ead3dac22e4d05
sergioreynoso <sergior@gmail.com>
2022-06-09 14:35:12 -0400

    Removed unused files

--- components/Expand/Expand.js
+++ components/Expand/Expand.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import useSWR from "swr";
-import { getTimeZone } from "../../utils/api";
 import {
   ANIMATION_TIME,
   COLORS,
