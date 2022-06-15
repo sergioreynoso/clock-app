2a9a4eaeb15a3732e086c3c9b68668efa0062a7e
sergioreynoso <sergior@gmail.com>
2022-06-15 12:45:32 -0400

    Cleaned up unused imports

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-import { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 import Head from "next/head";
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/700.css";
-import { ANIMATION_TIME, FADE_IN, QUERIES } from "../utils/constants";
 
 const DynamicComponentWithNoSSR = dynamic(
   () => import("../components/ClockApp/ClockApp"),
