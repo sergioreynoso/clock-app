b5ad8f37987be76b847d70a8ea2c05771de8b877
Sergio Reynoso <sergior@gmail.com>
2022-12-06 11:44:11 -0500

    chore: remove unneeded imports and files

--- components/ClockApp/ClockApp.js
+++ components/ClockApp/ClockApp.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import BackgroundImage from "../BackgroundImage";
 import Main from "../Main";
