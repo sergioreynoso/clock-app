9a78dce24dc70aa6760ddadffd36ee112bfa7617
Sergio Reynoso <sergior@gmail.com>
2022-12-06 12:06:39 -0500

    refactor: move Head tag to page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/700.css";
+import Head from "next/head";
 import ClockApp from "../components/ClockApp/ClockApp";
 
 const Home = () => {
-  return <ClockApp />;
+  return (
+    <>
+      <Head>
+        <title>Frontend Mentor - Clock App</title>
+      </Head>
+      <ClockApp />;
+    </>
+  );
 };
 
 export default Home;
