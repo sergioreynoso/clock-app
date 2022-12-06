ed0c4c35db1cfa9204bc5026561cda70ba5c88c4
Sergio Reynoso <sergior@gmail.com>
2022-12-06 12:53:14 -0500

    build: remove webpack svg config
    Using Next Image component to load svgs.

--- next.config.js
+++ next.config.js
@@ -1,22 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
   compiler: {
     styledComponents: true,
-  },
-  experimental: {
-    images: {
-      layoutRaw: true,
-    },
   },
 };
 
